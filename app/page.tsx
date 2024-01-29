e1cacdaa96fc369a918209b37eb8611ab6fc08e4
Elad Eckstein <eleckste@microsoft.com>
2024-01-29 21:56:26 +0200

    Add counter functionality to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,41 @@
+'use client'
 import Image from "next/image";
+import React from 'react'
 
 export default function Home() {
+  const [count, setCount] = React.useState(0);
+  //create the actions
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+  //create the counter
+  const Counter = () => (
+    <>
+      <button
+        onClick={decrement}
+      >
+        dec
+      </button>
+      <span className="text-2xl font-bold">{count}</span>
+      <button
+        onClick={increment}
+        >
+        inc
+      </button>
+    </>
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Elad's website
+      <Counter />
+      {/* render here the same example with actions and counter */}
+    </main>
+  );
+}
+
+// original inside-main-tag content
+/* 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
@@ -107,7 +140,4 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
-    </main>
-  );
-}
+      </div> */
